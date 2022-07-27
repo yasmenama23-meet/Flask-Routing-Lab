c3aad592976a8ff43a3f0dc050687c8ba6d51b6c
yasmena masalha <109899488+yasmenama23-meet@users.noreply.github.com>
2022-07-27 10:01:34 +0300

    k

--- Flask-Routing-Lab/app.py
+++ Flask-Routing-Lab/app.py
@@ -7,8 +7,13 @@ app = Flask(  __name__,
 )
 @app.route('/')
 def HELLO():
-        
-return render_template("home.html")
+    return render_template("home.html")
+@app.route('/c')
+def clothes():
+    return render_template("product.html")
+    @app.route('/')
+def HELLO():
+    return render_template("cart.html")
 
 if __name__ == "__main__": 
     app.run(debug=True)
